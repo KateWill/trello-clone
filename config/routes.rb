343eb3afb2e8844def15825b35475a4fda7e9680
Liv Cella <livncella@gmail.com>
2019-03-07 19:29:46 -0700

    'changed routes to resources :boards'

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,3 @@
 Rails.application.routes.draw do
-  get 'boards/new'
-  get 'boards/create'
-  get 'boards/update'
-  get 'boards/edit'
-  get 'boards/destroy'
-  get 'boards/index'
-  get 'boards/show'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :boards
 end
