1355f0db8fe5d2c6b3d9e9228199370cf72d4b50
Liv Cella <livncella@gmail.com>
2019-03-08 11:18:33 -0700

    fleshed out controllers, added some HTML

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,23 +1,48 @@
 class BoardsController < ApplicationController
 
+  def index
+    @boards = Board.all
+    @board = Board.new
+  end
+
   def new
+    @board = Board.new
   end
 
-  def create
-  end
-
-  def update
+  def show
   end
 
   def edit
   end
 
+  def create
+    @board = Board.new(board_params)
+    if @boardmodel.save
+      redirect_to boards_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    @board = Board.find_by(params[:id])
+    if @board.update(board_params)
+      redirect_to boards_path
+    else
+      render :edit
+    end
+  end
+
+
+
   def destroy
+    @board = Board.find_by(params[:id])
+    @board.destroy
+    redirect_to board_path
   end
 
-  def index
-  end
-
-  def show
-  end
+  private
+    def board_params
+      params.require(:board).permit(:name)
+    end
 end
