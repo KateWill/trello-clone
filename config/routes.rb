1aa83e5a1bfb9a95fe1ea477e54d414fe5a5b642
Liv Cella <livncella@gmail.com>
2019-03-07 19:27:19 -0700

    'added board model and controller'

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'boards/new'
+  get 'boards/create'
+  get 'boards/update'
+  get 'boards/edit'
+  get 'boards/destroy'
+  get 'boards/index'
+  get 'boards/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
